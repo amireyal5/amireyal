c71f63ef3819c4542ddaa3ac4e7cb50b1b923511
amireyal5 <amirher@gmail.com>
2025-04-25 16:41:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuLinks = mobileMenu.querySelectorAll('a');
     let prevScrollPos = window.pageYOffset; // מיקום הגלילה הקודם
 
+    // פונקציה להתאמת המיקום של התפריט
     function adjustMenuPosition() {
         if (window.innerWidth <= 768) {
             const headerHeight = header.offsetHeight;
@@ -14,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // הוספת אירועים לאייקון המבורגר ולתפריט
     if (hamburgerIcon && mobileMenu && menuLinks && header) {
         hamburgerIcon.addEventListener('click', function() {
             mobileMenu.classList.toggle('active');
@@ -82,10 +84,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         prevScrollPos = currentScrollPos;
     }
-});
-
-// JS - ניהול תפריט ההמבורגר
-document.getElementById('hamburger').addEventListener('click', function () {
-    var menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('active');  // הוספת/הסרת class שמראה/מסתיר את התפריט
 });
