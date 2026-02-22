316bd1e016ded7744b7e0172cefd89152848420c
עמיר אייל <amirher@gmail.com>
2026-02-22 09:59:46 +0000

    Convert to static site for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', images: { unoptimized: true }, 
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+const nextConfig = { output: 'export', images: { unoptimized: true } }; export default nextConfig;
